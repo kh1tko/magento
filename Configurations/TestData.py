a9b4e436d9b6464023af509eae72326a1e5ac34c
Artem <hitko21@gmail.com>
2024-07-01 15:04:10 +0300

    coding in progress....

---
+++ Configurations/TestData.py
@@ -0,0 +1,4 @@
+class TestData:
+    URL = 'https://magento.softwaretestingboard.com/'
+    USERNAME = 'kh11tko@gmail.com'
+    PASSWORD = 'Ukraine1991'
---
+++ Pages/BasePage.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BasePage:
+    def __init__(self):
+        self.driver = driver
+
+    def typeText_action(self, element, textType):
+        WebDriverWait(self.driver, 20).until(ec.visibility_of_element_located((element))).send_keys(textType)
+
+    def click_action(self, element):
+        WebDriverWait(self.driver, 20).until(ec.visibility_of_element_located((element))).click()
+
+    def get_elementText(self, element):
+        return WebDriverWait(self.driver, 20).until(ec.visibility_of_element_located((element))).text
+
+    def elementEnableStatus(self, element):
+        return WebDriverWait(self.driver, 20).until(ec.visibility_of_element_located((element))).is_enabled()
+
+    def elementDisplayStatus(self, element):
+        return WebDriverWait(self.driver, 20).until(ec.visibility_of_element_located((element))).is_displayed()
+
+    def get_pageTitle(self):
+        return self.driver.title
