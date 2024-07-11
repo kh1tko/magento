c265df40ac60dc794e249128ff3e559ecf1630cd
Artem <hitko21@gmail.com>
2024-07-11 22:36:01 +0300

    coding in progress....

--- Configurations/TestData.py
+++ Configurations/TestData.py
@@ -1,4 +1,6 @@
+import os
 class TestData:
     URL = 'https://magento.softwaretestingboard.com/'
     USERNAME = 'kh11tko@gmail.com'
     PASSWORD = 'Ukraine1991'
+    appEnv = os.environ.get('APP_TEST_ENVIRONMENT')
